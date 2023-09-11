95820549c1847f71c6ceb91ff21108d4ff3595a6
Dilsher Balouch <dilsherb@gmail.com>
2023-09-11 18:30:02 +0300

    rubocop linters corrected.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  devise  :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :validatable, :confirmable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :transactions, dependent: :nullify
 
   validates :name, presence: true
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }
   validates :confirm_password, presence: true, confirmation: true
 
-  after_create  :self.total_amt_for_user
+  after_create :self.total_amt_for_user
 
   def self.total_amt_for_user
     where(user_id:).sum(:amount)
